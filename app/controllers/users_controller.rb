5bff2ebb06582f173c6e2c63584d9876606ce1c7
Seongwan Park <synthresin@me.com>
2012-04-30 12:11:20 +0900

    Finished first cut of the User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  def new
-  	@title = "sign up"
-  end
+	def show
+		@user = User.find(params[:id])
+	end
+	def new
+  		@title = "sign up"
+	end
 end
