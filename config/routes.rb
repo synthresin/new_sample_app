1e71595a0d98735cdd685002a08ba22048d6276e
Seongwan Park <synthresin@me.com>
2012-05-10 16:17:04 +0900

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 SampleApp::Application.routes.draw do
+  
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match "/signup", :to => "users#new"
-  
+  match "/signin", :to => "sessions#new"
+  match "/signout", :to => "sessions#destroy"
+
   match "/about", :to => "pages#about"
   match "/help", :to => "pages#help"
   match "/contact", :to => "pages#contact"
